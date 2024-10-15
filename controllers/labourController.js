8983b604ff82c17f70c53f3dcece5f13febb913a
rahul <rahulsoftfix@gmail.com>
2024-10-15 17:12:12 +0530

    update

--- controllers/labourController.js
+++ controllers/labourController.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import { apiResponse } from "../utils/apiResponse.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
+import { asyncHandler } from "../utils/asynchandler.js";
 
 
 const uploadImage = asyncHandler(async (req, res) => {
@@ -22,18 +22,18 @@ const addLabour = asyncHandler(async (req, res) => {
     const { name, role, imageUrl } = req.body;
 
     // Validate request
-    if (!name || !role) {
-        return res.status(400).json({ message: "Name and role are required." });
-    }
+    // if (!name || !role) {
+    //     return res.status(400).json({ message: "Name and role are required." });
+    // }
 
-    try {
-        // Assuming you have a Labour model
-        const newLabour = new Labour({ name, role, imageUrl });
-        await newLabour.save();
-        res.status(201).json({ message: "Labour added successfully!", labour: newLabour });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+    // try {
+    //     // Assuming you have a Labour model
+    //     const newLabour = new Labour({ name, role, imageUrl });
+    //     await newLabour.save();
+    //     res.status(201).json({ message: "Labour added successfully!", labour: newLabour });
+    // } catch (error) {
+    //     res.status(500).json({ message: error.message });
+    // }
 });
 
 export {
